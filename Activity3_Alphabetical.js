8751305e1f69e1c8b598e0af953825dffad461b1
Edster29 <69906506+Edster29@users.noreply.github.com>
2022-10-20 23:26:25 +0800

    Delete Activity3_Alphabetical.js

--- Activity3_Alphabetical.js
+++
@@ -1,5 +0,0 @@
-function Alphabetical(string)// function that will process the string. 
-  {
-return string.split('').sort().join(''); // splitting the words into letter{split}, and then sorting them out in an alphabetical order{sort}, and combined them again but in a sorted position{join}. 
-  }
-console.log(Alphabetical("qwertyuiop"));// use the function to a word. 
